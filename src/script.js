7a381e9f1da9be6df53e61fecd5d19cbf489a016
Alessandro Silva <79567514+Alessandro-A-Silva@users.noreply.github.com>
2025-08-08 11:38:12 -0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,8 @@
+function CalcularIdade(){
+    const inputDataNascimento = document.getElementById("data-nascimento");
+    const dataNascimento = inputDataNascimento.value;
+    const paragrafo = document.getElementsByTagName()
+    if(dataNascimento == ""){
+        
+    }
+}
